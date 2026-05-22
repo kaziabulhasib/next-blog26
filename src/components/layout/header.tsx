c34694c0d65d0ac1f7fede66338e320a4658c3b5
kaziabulhasib <hasibkazi420@gmail.com>
2026-05-23 00:09:16 +0530

    feat: create header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,49 @@
-"use client"
+"use client";
 
-export default function header() {
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { useRouter } from "next/navigation";
+
+export default function Header() {
+  //  routers 
+  const navlinks = [
+    {
+      label: "home",
+      href: "/",
+    },
+
+    {
+      label: "post",
+      href: "/post",
+    },
+  ];
+
+  const router=useRouter()
   return (
-    <div>header</div>
-  )
+    <header className='flex justify-between items-center px-6 py-4 border-b bg-background' >
+      <div className='flex justify-between items-center gap-6'>
+        <div>
+          {" "}
+          <h1 className='text-2xl font-bold'>Next Blog</h1>{" "}
+        </div>
+        <div className='flex justify-between items-center gap-4'>
+          {navlinks.map((link) => {
+            return (
+              <Link key={link.href} href={link.href} className="capitalize">
+                {link.label}
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+      <div  className="hidden md:block">
+        {/* placeholder for search */}
+      </div>
+      {/* placeholdder for toggle theme  */}
+      <div>
+        <Button className="cursor-pointer"
+         onClick={() => router.push("/auth")}>Login</Button>
+      </div>
+    </header>
+  );
 }
